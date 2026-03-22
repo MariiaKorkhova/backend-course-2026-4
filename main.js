0a5eaa557782653b6c4be5fc88758da251ad3846
Mariia Korkhova bc2026-4 <mariia.korkhova@lnu.edu.ua>
2026-03-22 22:47:18 +0000

    Add part 2 logic with xml builder

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { program } = require('commander');
 const fs = require('fs');
+const { readFile, writeFile } = require('fs/promises');
 const http = require('http');
 const { XMLBuilder } = require('fast-xml-parser');
 
@@ -26,8 +27,55 @@ if (!fs.existsSync(options.input)) {
 }
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end("Server is created. Part 2 soon");
+    const reqUrl = new URL(req.url, `http://${options.host}:${options.port}`);
+    const filterSurvived = reqUrl.searchParams.get('survived') === 'true';
+    const showAge = reqUrl.searchParams.get('age') === 'true';
+
+    readFile(options.input, 'utf-8')
+        .then(data => {
+
+            const rawData = data.split('\n')
+                .filter(line => line.trim() !== '')
+                .map(line => JSON.parse(line));
+
+            const filteredPassengers = rawData
+                .filter(passenger => {
+                    if (filterSurvived) {
+                        return String(passenger.Survived) === "1" || passenger.Survived === true;
+                    }
+
+                    return true;
+                })
+                .map(passenger => {
+                    const xmlObj = { name: passenger.Name, ticket: passenger.Ticket };
+
+                    if (showAge && passenger.Age !== undefined && passenger.Age !== "") {
+                        xmlObj.age = passenger.Age;
+                    }
+
+                    return xmlObj;
+                });
+
+            const builder = new XMLBuilder({ format: true, ignoreAttributes: true });
+            const xmlStr = builder.build({ passengers: { passenger: filteredPassengers } });
+
+            res.setHeader('Content-Type', 'application/xml');
+            res.writeHead(200);
+            res.end(xmlStr);
+
+            return writeFile('latest_response.xml', xmlStr);
+        })
+        .then(() => {
+            console.log("log is saved successfully");
+        })
+        .catch(err => {
+            console.error("promises chain error:", err);
+
+            if (!res.headersSent) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end("Internal server error");
+            }
+        });
 });
 
 server.listen(options.port, options.host, () => {
